d2e0a386915d15930d6e7c35e39e46566f64b50e
okandemirel <okn.118dmrl@gmail.com>
2022-10-25 15:58:07 +0300

    Final Setup

--- Assets/Scripts/Data/UnityObjects/CD_Level.cs
+++ Assets/Scripts/Data/UnityObjects/CD_Level.cs
@@ -1,7 +1,8 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "CD_Level", menuName = "Picker3D/CD_Level")]
+[CreateAssetMenu(fileName = "CD_Level", menuName = "Picker3D/CD_Level", order = 1)]
 public class CD_Level : ScriptableObject
 {
     public List<LevelData> Levels = new List<LevelData>();
